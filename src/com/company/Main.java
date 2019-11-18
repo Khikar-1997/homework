3bbfa572d5cbef62cde64c4b1c9c6a10a6db4c85
Khikar-1997 <harutyunyan.xikar@bk.ru>
2019-11-18 21:09:29 +0400

    6_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String args[]) {
-        Triangle triangle = new Triangle(4, 8, 11);
-        if (triangle.isValidTriangle()) {
-            triangle.calculateTrianglePerimeter();
-            triangle.calculateTriangleArea();
-        }
+        Point point = new Point();
+
+        point.setCordinateX(0);
+        point.setCordinateY(6);
+
+        point.cordinantsDistance();
+        point.cordinantPointOneDistanceFromPointTwo(5, 0);
     }
 }
