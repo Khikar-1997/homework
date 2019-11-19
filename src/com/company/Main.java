5e36335df3fa005630bf561602531100e4d6afb0
Khikar-1997 <harutyunyan.xikar@bk.ru>
2019-11-19 11:22:03 +0400

    6_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String args[]) {
-        Point point = new Point(4,5);
+        Point point = new Point(0, 5);
 
         point.cordinantsDistance();
         point.cordinantPointOneDistanceFromPointTwo(5, 0);
--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -31,49 +31,13 @@ public class Point {
     //endregion
 
     public void cordinantsDistance() {
-        if (this.cordinateX != 0 && this.cordinateY != 0) {
-            double distance = Math.sqrt(Math.pow(this.cordinateX, 2) + Math.pow(this.cordinateY, 2));
-            System.out.println(distance);
-        } else if (this.cordinateX == 0) {
-            System.out.println(this.cordinateY);
-        } else {
-            System.out.println(this.cordinateX);
-        }
+        double distance = Math.sqrt(Math.pow(this.cordinateX, 2) + Math.pow(this.cordinateY, 2));
+        System.out.println(distance);
     }
 
     public void cordinantPointOneDistanceFromPointTwo(double cordinateX1, double cordinateY1) {
-        if (this.cordinateX == 0 && cordinateX1 == 0) {
-            double distance = this.cordinateY + cordinateY1;
-            System.out.println(distance);
-        } else if (this.cordinateX == 0 && cordinateX1 != 0) {
-            double distance = Math.sqrt(Math.pow((this.cordinateY - cordinateY1), 2) + Math.pow(cordinateX1, 2));
-            System.out.println(distance);
-        } else if (cordinateX1 == 0 && this.cordinateX != 0) {
-            double distance = Math.sqrt(Math.pow((this.cordinateY - cordinateY1), 2) + Math.pow(this.cordinateX, 2));
-            System.out.println(distance);
-        } else if (this.cordinateY == 0 && cordinateY1 == 0) {
-            double distance = this.cordinateX + cordinateX1;
-            System.out.println(distance);
-        } else if (this.cordinateY == 0 && cordinateY1 != 0) {
-            double distance = Math.sqrt(Math.pow((this.cordinateX - cordinateX1), 2) + Math.pow(cordinateY1, 2));
-            System.out.println(distance);
-        } else if (cordinateY1 == 0 && this.cordinateY != 0) {
-            double distance = Math.sqrt(Math.pow((this.cordinateX - cordinateX1), 2) + Math.pow(this.cordinateY, 2));
-            System.out.println(distance);
-        } else if (cordinateX1 != 0 && cordinateY1 != 0 && this.cordinateY != 0 && this.cordinateX != 0) {
-            double distance = Math.sqrt(Math.pow((this.cordinateY - cordinateY1), 2) + Math.pow((this.cordinateX - cordinateX1), 2));
-            System.out.println(distance);
-        } else if (this.cordinateX == 0 && cordinateY1 == 0) {
-            double distance = Math.sqrt(Math.pow(cordinateX1, 2) + Math.pow(this.cordinateY, 2));
-            System.out.println(distance);
-        } else if (cordinateX1 == 0 && this.cordinateY == 0) {
-            double distance = Math.sqrt(Math.pow(this.cordinateX, 2) + Math.pow(cordinateY1, 2));
-            System.out.println(distance);
-        } else if (this.cordinateX == 0 && this.cordinateY == 0) {
-            double distance = Math.sqrt(Math.pow(cordinateX1, 2) + Math.pow(cordinateY1, 2));
-            System.out.println(distance);
-        } else {
-            double distance = Math.sqrt(Math.pow(this.cordinateX, 2) + Math.pow(this.cordinateY, 2));
-        }
+
+        double distance = Math.sqrt(Math.pow((this.cordinateX - cordinateX1), 2) + Math.pow((this.cordinateY - cordinateY1), 2));
+        System.out.println(distance);
     }
 }
