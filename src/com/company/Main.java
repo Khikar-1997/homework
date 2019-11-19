9c99ebec5a3057568c7d155452f1eac93bfc6c0d
Khikar-1997 <harutyunyan.xikar@bk.ru>
2019-11-19 12:14:14 +0400

    6_6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,9 @@ package com.company;
 
 public class Main {
     public static void main(String args[]) {
-        TemperatureConverter temperatureConverter = new TemperatureConverter(10, 15, 18);
-        temperatureConverter.convertFarenheitToCelsius();
-        temperatureConverter.convertFarenheitToKelvin();
+        DistanceConverter distanceConverter = new DistanceConverter(5,100);
 
-        temperatureConverter.convertCelsiusToFarenheit();
-        temperatureConverter.convertCelsiusToKelvin();
-
-        temperatureConverter.convertKelvinToCelsius();
-        temperatureConverter.convertKelvinToFarenheit();
+        distanceConverter.convertMilesToMeters();
+        distanceConverter.convertMetersToMiles();
     }
 }
